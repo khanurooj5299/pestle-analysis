0014ea4aa6f0f87a427d0d0c65a558d91fcf4d37
urooj khan <urooj.khan@lelafe.com>
2024-03-19 10:01:30 +0530

    Created ScatterPlotComponent

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ScatterPlotComponent } from '../plots/scatter-plot/scatter-plot.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [ScatterPlotComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
---
+++ src/app/plots/scatter-plot/scatter-plot.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scatter-plot',
+  standalone: true,
+  imports: [],
+  templateUrl: './scatter-plot.component.html',
+  styleUrl: './scatter-plot.component.css'
+})
+export class ScatterPlotComponent {
+
+}
