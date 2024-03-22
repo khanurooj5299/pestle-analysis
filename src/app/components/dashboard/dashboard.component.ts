345625240af192d899c8d7577ab3e1921a1ce54e
urooj khan <urooj.khan@lelafe.com>
2024-03-22 13:48:09 +0530

    Added styling to plot-card

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { LinePlotComponent } from '../plots/line-plot/line-plot.component';
 import { ScatterPlotComponent } from '../plots/scatter-plot/scatter-plot.component';
+import { PlotCardComponent } from '../plots/plot-card/plot-card.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [LinePlotComponent, ScatterPlotComponent],
+  imports: [LinePlotComponent, ScatterPlotComponent, PlotCardComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
--- src/app/components/plots/plot-card/plot-card.component.ts
+++ src/app/components/plots/plot-card/plot-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-plot-card',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './plot-card.component.css'
 })
 export class PlotCardComponent {
-
+  @Input('title') cardTitle: string = '';
 }
