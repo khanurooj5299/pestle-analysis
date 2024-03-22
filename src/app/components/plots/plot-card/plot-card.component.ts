f78a409dfa67fac3e3bb439fe1363baa2373c5a3
urooj khan <urooj.khan@lelafe.com>
2024-03-22 12:14:33 +0530

    Added a new plot-card component in components/plots

---
+++ src/app/components/plots/plot-card/plot-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-plot-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './plot-card.component.html',
+  styleUrl: './plot-card.component.css'
+})
+export class PlotCardComponent {
+
+}
