88d9bd8814b5a74280b954ea542a8b7b69637b99
urooj khan <urooj.khan@lelafe.com>
2024-03-23 11:20:58 +0530

    Created layout on dashboard for many plots

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { LineScatterPlotComponent } from '../plots/line-scatter-plot/line-scatter-plot.component';
 import { PlotCardComponent } from '../plots/plot-card/plot-card.component';
+import { BarPlotComponent } from '../plots/bar-plot/bar-plot.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [LineScatterPlotComponent, PlotCardComponent],
+  imports: [LineScatterPlotComponent, PlotCardComponent, BarPlotComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
