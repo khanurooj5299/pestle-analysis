9d4d283c4827c8e065ae39418d749e652edf2a8f
urooj khan <urooj.khan@lelafe.com>
2024-03-23 10:51:37 +0530

    Created a bar-plot component

---
+++ src/app/components/plots/bar-plot/bar-plot.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bar-plot',
+  standalone: true,
+  imports: [],
+  templateUrl: './bar-plot.component.html',
+  styleUrl: './bar-plot.component.css'
+})
+export class BarPlotComponent {
+
+}
