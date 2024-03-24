64fbec6da38786bd53047ea9d72e0441bfe37b81
urooj khan <urooj.khan@lelafe.com>
2024-03-24 21:46:56 +0530

    Added layout to list-observations Added pagination to list-observations

--- src/app/components/list-observations/list-item-observation/list-item-observation.component.ts
+++ src/app/components/list-observations/list-item-observation/list-item-observation.component.ts
@@ -1,18 +1,20 @@
 import { Component, Input } from '@angular/core';
-import { KeyValuePipe } from '@angular/common';
+import { DatePipe, KeyValuePipe } from '@angular/common';
 import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 import { ObservationModel } from '../../../models/observation.model';
 
 @Component({
   selector: 'app-list-item-observation',
   standalone: true,
-  imports: [KeyValuePipe, MatDividerModule],
+  imports: [KeyValuePipe, MatDividerModule, DatePipe, MatButtonModule, MatIconModule],
   templateUrl: './list-item-observation.component.html',
   styleUrl: './list-item-observation.component.css'
 })
 export class ListItemObservationComponent {
   @Input('item') observationItem?: ObservationModel;
   //these fields of item are not displayed in the info area
-  infoExcludeFields = ["_id", "title", "insight", "__v"]
+  infoExcludeFields = ["_id", "title", "insight", "__v", "url"]
 }
--- src/app/components/list-observations/list-observations.component.ts
+++ src/app/components/list-observations/list-observations.component.ts
@@ -1,25 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ObservationModel } from '../../models/observation.model';
+import {MatPaginatorModule, PageEvent} from '@angular/material/paginator';
+
 import { DataService } from '../../services/data.service';
 import { ListItemObservationComponent } from './list-item-observation/list-item-observation.component';
 
 @Component({
   selector: 'app-list-observations',
   standalone: true,
-  imports: [ListItemObservationComponent],
+  imports: [ListItemObservationComponent, MatPaginatorModule],
   templateUrl: './list-observations.component.html',
   styleUrl: './list-observations.component.css',
 })
 export class ListObservationsComponent implements OnInit {
   observations: ObservationModel[] = [];
+  currentObservations: ObservationModel[] = [];
+  pageSize = 10;
 
   constructor(private dataService: DataService) {}
   ngOnInit(): void {
     this.dataService
       .getObservations()
-      .subscribe((observations) =>
-        observations.length ? (this.observations = observations) : ''
+      .subscribe((observations) => {
+        if(observations.length) {
+          this.observations = observations;
+          //for pagination on initial load
+          this.currentObservations = this.observations.slice(0, this.pageSize);
+        }
+      }
       );
+  }
+  paginate(pageEvent: PageEvent) {
+    this.pageSize = pageEvent.pageSize;
+    const newStartIndex = (pageEvent.pageIndex-1)*this.pageSize;
+    this.currentObservations = this.observations.slice(newStartIndex, newStartIndex + this.pageSize);
   }
 }
