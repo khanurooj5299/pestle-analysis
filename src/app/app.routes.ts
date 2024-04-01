64695e05dd1878261ef8bc4cc8f3a72580560b33
urooj khan <khanurooj5299@gmail.com>
2024-04-01 06:08:36 +0530

    Added new ErrorComponent and its path in routes file.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ListObservationsComponent } from './components/list-observations/list-observations.component';
+import { ErrorComponent } from './components/error/error.component';
 
 export const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'list-observations', component: ListObservationsComponent},
-  { path: '**', redirectTo: 'dashboard'},
+  { path: 'list-observations', component: ListObservationsComponent },
+  { path: 'error', component: ErrorComponent },
+  { path: '**', redirectTo: 'dashboard' },
 ];
