f505ba4172014256b192217d2d458fa559626531
urooj khan <khanurooj5299@gmail.com>
2024-04-01 11:39:51 +0530

    Added interceptor for network errors

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,41 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, inject } from '@angular/core';
+import { Router, provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {
+  HttpErrorResponse,
+  HttpEvent,
+  HttpHandlerFn,
+  HttpRequest,
+  provideHttpClient,
+  withInterceptors,
+} from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
 
 import { routes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([errorInterceptor])),
   ],
 };
+
+//Interceptor for network errors
+function errorInterceptor(
+  req: HttpRequest<unknown>,
+  next: HttpHandlerFn
+): Observable<HttpEvent<unknown>> {
+  const router = inject(Router);
+  return next(req).pipe(
+    catchError((err: HttpErrorResponse) => {
+      //for network error route to error page
+      if (err instanceof HttpErrorResponse && err.status == 0) {
+        router.navigate(['/error']);
+        return new Observable<HttpEvent<unknown>>();
+      }
+      //for other errors pass it along
+      return throwError(()=>err);
+    })
+  );
+}
